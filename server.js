bf2b859c7d9bd85ecd7081ff1e5686b863cffb95
Cristobal Mejia <cristobal.mejia@gmail.com>
2023-02-25 00:18:17 -0600

    Letss see

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ app.use(express.static('build', options))
 
 const port = process.env.PORT || 3000
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 app.listen(port, () => {
-  console.log(`React app listening at http://localhost:${port}`)
-})
+  //call();
+});
