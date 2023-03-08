1b60243f2fd4d2b7efefc45d78d919f1ad23a595
Cristobal Mejia <cristobal.mejia@gmail.com>
2023-03-08 01:28:31 -0600

    import

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
-const path = require('path');
-const express = require('express');
-
+import  epathxpress from "path";
+import  express from "express";
 
 const cUp = chalk.green;
 const cDown = chalk.red;
@@ -25,76 +24,3 @@ app.listen(port, () => {
   //call();
 });
 
-
-/*
-//setInterval(checkprice, (intervalo*1000));
-checkprice();
-const intervalprice = setInterval(() => {
-    checkprice();
-  }, intervalo*1000);
-
-
-
-
-function rendertable() {
-    return "<div>    </div>";
-}
-
-
-function checkprice() {
-    const options = {
-    method: 'GET',
-    url: 'https://api.coingecko.com/api/v3/simple/price',
-    qs: {ids: token, vs_currencies: currency},
-    json: true,
-    headers: {
-        useQueryString: true
-    }
-    };
-
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        const now  =  new Date();
-        const timestamp = date.format(now,'HH:mm:ss');
-        if(Object.keys(body).length !== 0) {
-           const resp = body[token];
-            var currentprice = resp[currency];
-            verifica(timestamp, currentprice);
-        }
-        else {
-            console.log(response);
-        }
-        checkpoint++;
-    });
-}
-
-function verifica(timestamp, currentprice) {
-    var entry = {};
-    entry.lastprice = lastprice;
-    entry.timestamp = timestamp;
-    entry.currentprice  = currentprice;
-    entry.variacion = 0;
-    entry.porcentaje = 0;
-    var output = `${checkpoint} ${timestamp} `; 
-    if (entry.lastprice != 0) {
-        entry.variacion = entry.currentprice-entry.lastprice;
-        if (entry.variacion == 0) {
-            output+= entry.currentprice;
-        }
-        else if (entry.variacion>0) {
-            entry.porcentaje = (entry.variacion*100)/entry.currentprice;
-
-            output = output + cUp(entry.currentprice) + " " + cUp("%") + cUp(parseFloat(entry.porcentaje).toFixed(2)) + " ("+ cUp(entry.variacion)+")";
-        }
-        else {
-            entry.porcentaje = ((entry.variacion)*100)/entry.currentprice;
-
-            output = output + cDown(entry.currentprice)+ " "+cDown("-%") + cDown((-1)*parseFloat(entry.porcentaje).toFixed(2)) +  " ("+ cDown(entry.variacion)+")";
-        }
-    }
-
-    //console.log(entry);
-    console.log(output);
-    lastprice = currentprice;
-}
-*/
